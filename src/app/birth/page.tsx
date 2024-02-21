033b2c7506a0d6054ce6a668ec7e228f3cbf5411
Astha Thakur <128837575+aasthathakor@users.noreply.github.com>
2024-02-21 17:20:44 +0530

    commit 3

--- src/app/birth/page.tsx
+++ src/app/birth/page.tsx
@@ -64,6 +64,7 @@ export default function Talati() {
           onSubmit={form.handleSubmit(handleSubmit)}
           className="max-w-md w-full flex flex-col gap-4"
         >
+          <FormLabel className="flex flex-col items-center justify-between text-3xl font-bold" >Birth</FormLabel>
           <FormField
             control={form.control}
             name="birth_id"
